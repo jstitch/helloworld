f831005818c7e6be9a30e42021371852012c21f6
Javier Novoa <jstitch@gmail.com>
2012-02-16 21:20:53 -0600

    Implemento web
    Utilizamos Django para la interfaz web.
    
    El codigo hace un poco de trampa, la plantilla solo pinta 'Hello
    World!' directamente sin utilizar la libreria, eso por razones del
    demo que se esta armando en este momento...

---
+++ urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls.defaults import patterns, include, url
+from views import hello
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+#                       url(r'^hello/(?P<lang>\w{2})/$', hello),
+                       url(r'^$', hello),
+)
